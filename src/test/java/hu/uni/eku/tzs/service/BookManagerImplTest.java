7ae40a3233acd953234977ab7335a15a0fa5e34a
Zsolt Toth <tothzs87@gmail.com>
2021-08-23 12:34:33 +0200

    test(#9): increase code coverage

--- src/test/java/hu/uni/eku/tzs/service/BookManagerImplTest.java
+++ src/test/java/hu/uni/eku/tzs/service/BookManagerImplTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,24 +33,12 @@ class BookManagerImplTest {
     BookManagerImpl service;
 
     @Test
-    public void recordBookHappyPath() throws BookAlreadyExistsException {
+    void recordBookHappyPath() throws BookAlreadyExistsException {
         // given
-        Author douglasAdams = new Author(1, "Douglas", "Adams", "English");
-        AuthorEntity douglasAddamsEntity = AuthorEntity.builder()
-                .id(1)
-                .firstName("Douglas")
-                .lastName("Adams")
-                .nationality("English")
-                .build();
-        final String HG2G_ISBN = "1-85695-028-X";
-        final String HG2G_TITLE = "The Hitchhiker's Guide to the Galaxy";
-        Book hg2g = new Book(HG2G_ISBN, douglasAdams, HG2G_TITLE, "English");
-        BookEntity hg2gEntity = BookEntity.builder()
-                .isbn(HG2G_ISBN)
-                .title(HG2G_TITLE)
-                .author(douglasAddamsEntity)
-                .language("English")
-                .build();
+        Author douglasAdams = BookDataProvider.getDouglasAdamsModel();
+        AuthorEntity douglasAddamsEntity = BookDataProvider.getDouglasAdamsEntity();
+        Book hg2g = BookDataProvider.getHitchhikersGuide();
+        BookEntity hg2gEntity = BookDataProvider.getHitchhikersGuideEntity();
         when(bookRepository.findById(any())).thenReturn(Optional.empty());
         when(authorRepository.findById(douglasAdams.getId())).thenReturn(Optional.ofNullable(douglasAddamsEntity));
         when(bookRepository.save(any())).thenReturn(hg2gEntity);
@@ -58,5 +47,49 @@ class BookManagerImplTest {
         // then
         assertThat(actual).usingRecursiveComparison().isEqualTo(hg2g);
 //        assertThat(actual).isEqualToComparingFieldByFieldRecursively(hg2g);
+    }
+
+    @Test
+    void recordBookAlreadyExistsException() {
+        // given
+        Book hg2g = BookDataProvider.getHitchhikersGuide();
+        BookEntity hg2gEntity = BookDataProvider.getHitchhikersGuideEntity();
+        when(bookRepository.findById(BookDataProvider.HG2G_ISBN)).thenReturn(Optional.ofNullable(hg2gEntity));
+        // when
+        assertThatThrownBy(()->{
+            service.record(hg2g);
+        }).isInstanceOf(BookAlreadyExistsException.class);
+    }
+
+    private static class BookDataProvider{
+
+        public static final String HG2G_ISBN = "1-85695-028-X";
+        public static final String HG2G_TITLE = "The Hitchhiker's Guide to the Galaxy";
+
+        public static Author getDouglasAdamsModel(){
+            return new Author(1, "Douglas", "Adams", "English");
+        }
+
+        public static AuthorEntity getDouglasAdamsEntity(){
+            return AuthorEntity.builder()
+                .id(1)
+                .firstName("Douglas")
+                .lastName("Adams")
+                .nationality("English")
+                .build();
+        }
+
+       public static Book getHitchhikersGuide(){
+           return new Book(HG2G_ISBN, getDouglasAdamsModel(), HG2G_TITLE, "English");
+       }
+
+       public static BookEntity getHitchhikersGuideEntity(){
+           return BookEntity.builder()
+               .isbn(HG2G_ISBN)
+               .title(HG2G_TITLE)
+               .author(getDouglasAdamsEntity())
+               .language("English")
+               .build();
+       }
     }
 }
