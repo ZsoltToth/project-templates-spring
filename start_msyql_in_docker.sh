e2849c9bcb496d71899d116fdbd8f5a76b9280d2
Zsolt Toth <tothzs87@gmail.com>
2021-11-22 08:54:36 +0100

    refactor(docker): wrap docker run arguments into an array

--- start_msyql_in_docker.sh
+++ start_msyql_in_docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-docker run \
- --env MYSQL_ROOT_PASSWORD=secret \
- --env MYSQL_DATABASE=library \
- --name mysql \
- --publish 3306:3306 \
- -rm \
- mysql:8
+DOCKER_RUN_ARGS=()
+DOCKER_RUN_ARGS+=(--env MYSQL_ROOT_PASSWORD=secret)
+DOCKER_RUN_ARGS+=(--env MYSQL_DATABASE=library)
+DOCKER_RUN_ARGS+=(--name mysql)
+DOCKER_RUN_ARGS+=(--publish 3306:3306)
+DOCKER_RUN_ARGS+=(--rm)
+# shellcheck disable=SC2068
+docker run ${DOCKER_RUN_ARGS[@]} mysql:8
